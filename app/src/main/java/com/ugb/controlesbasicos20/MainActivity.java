e23492bf7a3ac340b449b7da5c8ccd9229a317bb
EzequielDiaz00 <arashixz00@gmail.com>
2024-03-01 13:25:00 -0600

    Practica SQLite

--- app/src/main/java/com/ugb/controlesbasicos20/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos20/MainActivity.java
@@ -14,9 +14,45 @@ import com.ugb.controlesbasicos20.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tempVal;
+    Button btn;
+    String id="", accion="nuevo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtnombre);
+                String nombre = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtdireccion);
+                String direccion = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtTelefono);
+                String tel = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtEmail);
+                String email = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtDui);
+                String dui = tempVal.getText().toString();
+
+                String[] datos = new String[]{id, nombre, direccion, tel, email, dui};
+
+                DB db = new DB(getApplicationContext(), "", null, 1);
+                String respuesta = db.administrar_amigos(accion, datos);
+
+                if(respuesta.equals("ok")){
+                    Toast.makeText(getApplicationContext(), "Amigo resgistrado con exito", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Error al registrar el amigo" + respuesta, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
