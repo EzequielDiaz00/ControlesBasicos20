21ea08eef465b06c4fe0eaff31c8403d6ac4e566
EzequielDiaz00 <arashixz00@gmail.com>
2024-03-20 23:01:00 -0600

    Tienda Android

--- app/src/main/java/com/ugb/controlesbasicos20/DB.java
+++ app/src/main/java/com/ugb/controlesbasicos20/DB.java
@@ -1,16 +1,17 @@
 package com.ugb.controlesbasicos20;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DB extends SQLiteOpenHelper {
-    private static final String dbname="amigos";
+    private static final String dbname="TiendaSQLite";
     private static final int v=1;
-    private static final String SQLdb = "CREATE TABLE amigos (idAmigo integer primary key autoincrement, nombre text, direccion text, telefono text, " +
-            "email text, dui text)";
+    private static final String SQLdb = "CREATE TABLE productos (idProd integer primary key autoincrement, codigo text, " +
+            "descripcion text, marca text, presentacion text, precio text, foto text)";
     public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, dbname, factory, v);
     }
@@ -22,21 +23,28 @@ public class DB extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         //actualizar la estrucutra de la BD.
     }
-    public String administrar_amigos(String accion, String[] datos){
+    public String administrar_prod(String accion, String[] datos){
         try {
             SQLiteDatabase db = getWritableDatabase();
             String sql = "";
-            if (accion == "nuevo") {
-                sql = "INSERT INTO amigos(nombre,direccion,telefono,email,dui) VALUES('" + datos[1] + "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "')";
-            } else if (accion == "modificar") {
-                sql = "UPDATE amigos SET nombre='" + datos[1] + "',direccion='" + datos[2] + "',telefono='" + datos[3] + "',email='" + datos[4] + "',dui='" + datos[5] + "' WHERE idAmigo='" + datos[0] + "'";
-            } else if (accion == "eliminar") {
-                sql = "DELETE FROM amigos WHERE idAmigo='" + datos[0] + "'";
+            if (accion.equals("nuevo")) {
+                sql = "INSERT INTO productos (codigo,descripcion,marca,presentacion,precio, foto) VALUES('" + datos[1] +
+                        "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "', '"+ datos[6] +"')";
+            } else if (accion.equals("modificar")) {
+                sql = "UPDATE productos SET codigo='" + datos[1] + "',descripcion='" + datos[2] + "',marca='" +
+                        datos[3] + "',presentacion='" + datos[4] + "',precio='" + datos[5] + "', foto='"+ datos[6] +"' WHERE idProd='" + datos[0] + "'";
+            } else if (accion.equals("eliminar")) {
+                sql = "DELETE FROM productos WHERE idProd='" + datos[0] + "'";
             }
             db.execSQL(sql);
             return "ok";
         }catch (Exception e){
             return e.getMessage();
         }
+    }
+    public Cursor consultar_prod(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM productos ORDER BY codigo", null);
+        return cursor;
     }
 }
--- app/src/main/java/com/ugb/controlesbasicos20/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos20/MainActivity.java
@@ -1,74 +1,167 @@
 package com.ugb.controlesbasicos20;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TabHost;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.ugb.controlesbasicos20.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
     TextView tempVal;
     Button btn;
-    String id="", accion="nuevo";
     FloatingActionButton btnRegresar;
+    String id="", accion="nuevo";
+    ImageView img;
+    String urlCompletaFoto;
+    Intent tomarFotoIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRegresar = findViewById(R.id.fabVerAmigos);
+        btnRegresar = findViewById(R.id.fabListaProd);
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                abrirActividad();
+            public void onClick(View view) {
+                Intent regresarLista = new Intent(getApplicationContext(), ProductosActivity.class);
+                startActivity(regresarLista);
             }
         });
-
-        btn = findViewById(R.id.btnGuardarAmigo);
+        btn = findViewById(R.id.btnGuardarProd);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                tempVal = findViewById(R.id.txtnombre);
-                String nombre = tempVal.getText().toString();
+            public void onClick(View view) {
+                tempVal = findViewById(R.id.txtCod);
+                String codigo = tempVal.getText().toString();
 
-                tempVal = findViewById(R.id.txtdireccion);
-                String direccion = tempVal.getText().toString();
+                tempVal = findViewById(R.id.txtDes);
+                String descripcion = tempVal.getText().toString();
 
-                tempVal = findViewById(R.id.txtTelefono);
-                String tel = tempVal.getText().toString();
+                tempVal = findViewById(R.id.txtMar);
+                String marca = tempVal.getText().toString();
 
-                tempVal = findViewById(R.id.txtEmail);
-                String email = tempVal.getText().toString();
+                tempVal = findViewById(R.id.txtPres);
+                String presentacion = tempVal.getText().toString();
 
-                tempVal = findViewById(R.id.txtDui);
-                String dui = tempVal.getText().toString();
+                tempVal = findViewById(R.id.txtPrec);
+                String precio = tempVal.getText().toString();
 
-                String[] datos = new String[]{id, nombre, direccion, tel, email, dui};
-
-                DB db = new DB(getApplicationContext(), "", null, 1);
-                String respuesta = db.administrar_amigos(accion, datos);
-
-                if(respuesta.equals("ok")){
-                    Toast.makeText(getApplicationContext(), "Amigo resgistrado con exito", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Error al registrar el amigo" + respuesta, Toast.LENGTH_LONG).show();
+                String[] datos = new String[]{id,codigo,descripcion,marca,presentacion,precio, urlCompletaFoto};
+                DB db = new DB(getApplicationContext(),"", null, 1);
+                String respuesta = db.administrar_prod(accion, datos);
+                if( respuesta.equals("ok") ){
+                    mostrarMsg("Producto registrado con exito.");
+                    listarProd();
+                }else {
+                    mostrarMsg("Error al intentar registrar el producto: "+ respuesta);
                 }
             }
         });
+        img = findViewById(R.id.btnImgProd);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFotoProd();
+            }
+        });
+        mostrarDatosProd();
     }
-    private void abrirActividad(){
-        Intent abrirActividad = new Intent(getApplicationContext(), lista_amigos.class);
-        startActivity(abrirActividad);
+    private void tomarFotoProd(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fotoProd = null;
+        try{
+            fotoProd = crearImagenamigo();
+            if( fotoProd!=null ){
+                Uri urifotoAmigo = FileProvider.getUriForFile(MainActivity.this,
+                        "com.ugb.controlesbasicos.fileprovider", fotoProd);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, urifotoAmigo);
+                startActivityForResult(tomarFotoIntent, 1);
+            }else{
+                mostrarMsg("No se pudo tomar la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al abrir la camara"+ e.getMessage());
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if( requestCode==1 && resultCode==RESULT_OK ){
+                Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageBitmap(imagenBitmap);
+            }else{
+                mostrarMsg("Se cancelo la toma de la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al seleccionar la foto"+ e.getMessage());
+        }
+    }
+    private File crearImagenamigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+                fileName = "imagen_"+fechaHoraMs+"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdirs();
+        }
+        File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaFoto = image.getAbsolutePath();
+        return image;
+    }
+    private void mostrarDatosProd(){
+        try{
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+
+            if(accion.equals("modificar")){
+                String[] productos = parametros.getStringArray("productos");
+                id = productos[0];
+
+                tempVal = findViewById(R.id.txtCod);
+                tempVal.setText(productos[1]);
+
+                tempVal = findViewById(R.id.txtDes);
+                tempVal.setText(productos[2]);
+
+                tempVal = findViewById(R.id.txtMar);
+                tempVal.setText(productos[3]);
+
+                tempVal = findViewById(R.id.txtPres);
+                tempVal.setText(productos[4]);
+
+                tempVal = findViewById(R.id.txtPrec);
+                tempVal.setText(productos[5]);
+
+                urlCompletaFoto = productos[6];
+                Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageBitmap(imagenBitmap);
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar los datos amigos");
+        }
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void listarProd(){
+        Intent intent = new Intent(getApplicationContext(), ProductosActivity.class);
+        startActivity(intent);
     }
 }
